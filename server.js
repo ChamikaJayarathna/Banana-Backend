52e714ae726c7a6c22670398566af130f384c0f4
Chamika Jayarathna <failgames.lk@gmail.com>
2024-10-16 16:51:02 +0530

    create mongodb database connection

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import 'dotenv/config';
 
 const app = express();
 let PORT = 3000;
+
+mongoose.connect(process.env.MY_DB_LOCATION)
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch(err => {
+    console.log(err);
+})
+
+
+app.use(express.json());
 
 
 app.listen(PORT, () => {
