8cfc02d03e25a34824926df5d7c3352cd5bc3b32
Chamika Jayarathna <failgames.lk@gmail.com>
2024-10-24 21:54:49 +0530

    create user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    score: {
+        type: Number,
+        default: 0
+    },
+    profile_img:{
+        type:String,
+        default: () => {
+            `https://api.dicebear.com/6.x/${profile_imgs_collections_list[Math.floor(Math.random() * profile_imgs_collections_list.length)]}/svg?seed=${profile_imgs_name_list[Math.floor(Math.random() * profile_imgs_name_list.length)]}`
+        }
+    }
+});
+
+
+export default mongoose.model('user', UserSchema)
