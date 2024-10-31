926ab7fe489f89e1bd5546821c885b5d7c52156b
Chamika Jayarathna <failgames.lk@gmail.com>
2024-10-31 19:32:38 +0530

    add question route to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from 'cors';
 import 'dotenv/config';
 import userRoutes from './routes/user.js';
+import questionRoutes from './routes/question.js';
 
 const app = express();
 let PORT = 3000;
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api', userRoutes);
+app.use('/api', questionRoutes);
 
 
 app.listen(PORT, () => {
