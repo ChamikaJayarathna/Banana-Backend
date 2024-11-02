fdb59a6ff48ca86eb3cad77ef15b1180a12207de
Chamika Jayarathna <failgames.lk@gmail.com>
2024-11-02 19:15:34 +0530

    create score route

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 import User from "../models/User.js";
+import VefifyAuth from '../middleware/verifyJWT.js';
 
 const router = express.Router();
 
@@ -71,6 +72,29 @@ router.post('/login', async (req, res) => {
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
+});
+
+router.put('/score', VefifyAuth, async (req, res, ) => {
+
+    const user_id = req.body._id;
+    const { score } = req.body;
+
+    try {
+        const user = await User.findById(user_id);
+
+        if(!user){
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        user.score = parseInt(score);
+        await user.save();
+
+        res.status(200).json({ message: "Game Score updated", score: user.score });
+
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+
 });
 
 export default router;
